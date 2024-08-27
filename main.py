ec6dae2bfe72805a95fcd210d269d3dc23e8f43d
StudiosDanilIs <dalviarezhernandez@gmail.com>
2024-08-27 13:10:44 -0400

    .

--- main.py
+++ main.py
@@ -7,33 +7,33 @@ class GitHubDownloader:
         self.root = root
         self.root.title("GitHub Downloader")
         self.root.geometry("400x300")
-        self.root.configure(bg="#2c3e50")
+        self.root.configure(bg="#1778FB")
 
         # Estilo
         ctk.set_appearance_mode("dark")  # Modo oscuro
         ctk.set_default_color_theme("blue")  # Tema de color
 
         # Etiqueta
-        self.label = ctk.CTkLabel(root, text="Descargar aplicación desde GitHub", font=("Helvetica", 16))
+        self.label = ctk.CTkLabel(root, text="Descargar Aplicacion para Tempus", font=("Helvetica", 16))
         self.label.pack(pady=20)
 
         # Opciones de descarga
-        self.option_var = ctk.StringVar(value="Seleccione una opción")
-        self.option_menu = ctk.CTkOptionMenu(root, variable=self.option_var, values=["Opción 1", "Opción 2"])
+        self.option_var = ctk.StringVar(value="Opciones a Descargar")
+        self.option_menu = ctk.CTkOptionMenu(root, variable=self.option_var, values=["Tempus Gestión", "Tempus Reset"], fg_color="#1778FB", text_color="#000000")
         self.option_menu.pack(pady=10)
 
         # Botón de descarga
-        self.download_button = ctk.CTkButton(root, text="Descargar", command=self.download_file)
+        self.download_button = ctk.CTkButton(root, text="Descargar", command=self.download_file, fg_color="#1778FB", text_color="#000000")
         self.download_button.pack(pady=20)
 
     def download_file(self):
         option = self.option_var.get()
-        if option == "Opción 1":
+        if option == "Tempus Gestión":
             url = "https://raw.githubusercontent.com/StudiosDanilIs/Tempus_Tkinter/main/gato.exe"
-            local_filename = "archivo1.exe"
-        elif option == "Opción 2":
+            local_filename = "Tempus Gestión.exe"
+        elif option == "Tempus Reset":
             url = "https://raw.githubusercontent.com/StudiosDanilIs/Tempus_Tkinter/main/perro.exe"
-            local_filename = "archivo2.exe"
+            local_filename = "Tempus Reset.exe"
         else:
             messagebox.showerror("Error", "Por favor, seleccione una opción válida.")
             return
